748bf0a9b9f5d7600221077f1efafd6461261cf0
Adis Durakovic <adis.durakovic@gmail.com>
2014-12-07 14:32:35 +0100

    Code improvement

--- HuiDriver/HuiDriver/main.c
+++ HuiDriver/HuiDriver/main.c
@@ -40,6 +40,8 @@ Pres. = [7][6]
 #define PENBUTTON_BUTTON2 196
 #define PENBUTTON_TIPBUTTON2 197
 
+#define SLEEPTIME 1200
+
 #include <stdio.h>
 #include <wchar.h>
 #include <string.h>
@@ -67,88 +69,82 @@ int xPen; // x-position of pen
 int yPen; // y-position of pen
 int pPen; // pressure of pen
 
+double xRatio;
+double pRatio;
+
 
 double screenWidth;
 double screenHeight;
 
 int buttonLeftDown = 0;
 
-CGEventRef moveMouse, clickLeftMouse, releaseLeftMouse;
+CGEventRef moveMouse, dragMouse, clickLeftMouse, releaseLeftMouse, mouseEvent;
 
 
 struct hid_device_info;
+double tabletSpan;
+double screenSpan;
 
-double calculateYMouse(int xPen) {
-    if(xPen < 100) {
-        return -1;
-    }
-    if(xPen > (SENSITIVITY_LEVELS - CLEANAREA_PADDING_BOTTOM)) {
-        return screenHeight-0.999999;
-    }
-    double tabletSpan = (SENSITIVITY_LEVELS-(CLEANAREA_PADDING_TOP + CLEANAREA_PADDING_BOTTOM));
-    double screenSpan = screenHeight-1; // aproximate mouse height
-
-    double valueScaled = (xPen - 100) / tabletSpan;
-
-    printf("%f", (valueScaled * screenSpan));
-    return valueScaled * screenSpan;
-}
+double valueScaled;
 
 
-void mouseHandle(double x, double y, double pressure, int penButton) {
+/**
+* move mouse without touching the tablet
+*/
+void doMouseMove(double x, double y) {
 
-//    printf(".");
+    CGEventSetLocation(moveMouse, CGPointMake(x, y));
+    CGEventSetLocation(releaseLeftMouse, CGPointMake(x, y));
 
-    if(penButton == PENBUTTON_NONE) {
-        // move mouse with pen over the tablet
-        moveMouse = CGEventCreateMouseEvent(
-                NULL, kCGEventMouseMoved,
-                CGPointMake(x, y),
-                kCGMouseButtonLeft // ignored
-        );
-
-        releaseLeftMouse = CGEventCreateMouseEvent(
-                NULL, kCGEventLeftMouseUp,
-                CGPointMake(x, y),
-                kCGMouseButtonLeft
-        );
-
-        if(buttonLeftDown == 1) {
-            buttonLeftDown = 0;
-            CGEventPost(kCGHIDEventTap, releaseLeftMouse);
-            CFRelease(releaseLeftMouse);
-        }
-
-    } else if (penButton == PENBUTTON_TIP) {
-        moveMouse = CGEventCreateMouseEvent(
-                NULL, kCGEventLeftMouseDragged,
-                CGPointMake(x, y),
-                kCGMouseButtonLeft // ignored
-        );
-
-        clickLeftMouse = CGEventCreateMouseEvent(
-                NULL, kCGEventLeftMouseDown,
-                CGPointMake(x, y),
-                kCGMouseButtonLeft
-        );
-
-        CGEventSetDoubleValueField(moveMouse, kCGMouseEventPressure, pressure);
-        CGEventSetIntegerValueField(moveMouse, kCGMouseEventSubtype, kCGEventMouseSubtypeTabletPoint);
-        CGEventSetIntegerValueField(moveMouse, kCGTabletEventPointPressure, pressure);
-
-
-        CGEventSetDoubleValueField(clickLeftMouse, kCGMouseEventPressure, pressure);
-        CGEventSetIntegerValueField(clickLeftMouse, kCGMouseEventSubtype, kCGEventMouseSubtypeTabletPoint);
-        CGEventSetIntegerValueField(clickLeftMouse, kCGTabletEventPointPressure, pressure);
-
-        if(buttonLeftDown == 0) {
-            buttonLeftDown = 1;
-            CGEventPost(kCGHIDEventTap, clickLeftMouse);
-        }
-
+    if(buttonLeftDown == 1) {
+        buttonLeftDown = 0;
+        CGEventSetLocation(releaseLeftMouse, CGPointMake(x, y));
+        CGEventPost(kCGHIDEventTap, releaseLeftMouse);
+//        CFRelease(releaseLeftMouse);
+//        CFRelease(dragMouse);
     }
 
     CGEventPost(kCGHIDEventTap, moveMouse);
+}
+
+void doMouseMoveDrag(double x, double y, double pressure) {
+
+    CGEventSetLocation(dragMouse, CGPointMake(x, y));
+
+
+    CGEventSetDoubleValueField(dragMouse, kCGMouseEventPressure, pressure);
+    CGEventSetIntegerValueField(dragMouse, kCGMouseEventSubtype, kCGEventMouseSubtypeTabletPoint);
+//    CGEventSetIntegerValueField(dragMouse, kCGTabletEventPointPressure, pressure);
+
+
+    CGEventSetDoubleValueField(clickLeftMouse, kCGMouseEventPressure, pressure);
+    CGEventSetIntegerValueField(clickLeftMouse, kCGMouseEventSubtype, kCGEventMouseSubtypeTabletPoint);
+//    CGEventSetIntegerValueField(clickLeftMouse, kCGTabletEventPointPressure, pressure);
+    if(buttonLeftDown == 0) {
+//        printf("here\n");
+        buttonLeftDown = 1;
+        CGEventSetLocation(clickLeftMouse, CGPointMake(x, y));
+        CGEventPost(kCGHIDEventTap, clickLeftMouse);
+    }
+//
+    CGEventPost(kCGHIDEventTap, dragMouse);
+}
+
+void handleMouse(double x, double y, double pressure, int penButton) {
+
+    if (penButton == PENBUTTON_TIP) {
+        doMouseMoveDrag(x, y, pressure);
+    } else if (penButton == PENBUTTON_BUTTON1) {
+
+    } else if (penButton == PENBUTTON_TIPBUTTON1) {
+
+    } else if (penButton == PENBUTTON_BUTTON2) {
+
+    } else if (penButton == PENBUTTON_TIPBUTTON2) {
+
+    } else {
+        doMouseMove(x, y);
+    }
 
 
 }
@@ -159,7 +155,8 @@ void mouseHandle(double x, double y, double pressure, int penButton) {
 * calcPenValue (a, b)
 *
 * calculates the value coming from the pen for x, y and pressure
-* result min: 0, max: 2048
+* if a exceeds 255 it's set back to 0 and b gets increased by 1 resulting in a range of 2048 values
+* return min: 0, max: 2048
 *
 * a = 0-255
 * b = 0-7
@@ -170,19 +167,79 @@ double calcPenValue(int a, int b) {
     return a + (256*b);
 }
 
+double calculateYMouse(int xPen) {
+    if(xPen < 100) {
+        return -1;
+    }
+    if(xPen > (SENSITIVITY_LEVELS - CLEANAREA_PADDING_BOTTOM)) {
+        return screenHeight-0.999999;
+    }
+    tabletSpan = (SENSITIVITY_LEVELS-(CLEANAREA_PADDING_TOP + CLEANAREA_PADDING_BOTTOM));
+    screenSpan = screenHeight-1; // aproximate mouse height
+
+    valueScaled = (xPen - 100) / tabletSpan;
+
+    // handle left hand mode
+
+    return valueScaled * screenSpan;
+}
+
+double calculateXMouse(int xPen) {
+    // handle left-hand mode
+    return xRatio * xPen;
+}
+
+double calculatePMouse(int pPen) {
+    // handle sensitivity
+    return pRatio * pPen;
+}
+
+
+void handleTablet() {
+
+    res = hid_read(handle, buf, sizeof(buf));
+    usleep(SLEEPTIME);
+    // Print out the returned buffer.
+//    printf("%d\n", res);
+    if(res > 0) {
+        for (i = 0; i < res; i++) {
+//
+            xPen = calcPenValue((int) buf[2], (int) buf[3]);
+            yPen = calcPenValue((int) buf[4], (int) buf[5]);
+            pPen = calcPenValue((int) buf[6], (int) buf[7]);
+//
+////          printf("%d %d\n", yPen);
+            if(calculateYMouse(yPen) > 0) {
+                xMouse = calculateXMouse(xPen);
+                yMouse = calculateYMouse(yPen);
+                pMouse = calculatePMouse(pPen);
+
+                handleMouse(xMouse, yMouse, pMouse, (int) buf[1]);
+            }
+////            printf("mousepos: %d, %f, %f, %d, %f, %f \n",xPen, xRatio, xMouse, yPen, yRatio, yMouse);
+//            printf("%02hhx ", buf[i]);
+        }
+////        printf("\n");
+    }
+
+
+
+//    hid_close(handle);
+
+    /* Free static HIDAPI objects. */
+//    hid_exit();
+}
+
 
 
 int main(int argc, char* argv[]) {
 
 
-
-
     screenWidth = CGDisplayPixelsWide(CGMainDisplayID());
     screenHeight = CGDisplayPixelsHigh(CGMainDisplayID());
 
-    double xRatio = screenWidth/SENSITIVITY_LEVELS;
-    double yRatio = screenHeight/(SENSITIVITY_LEVELS - 148);
-    double pRatio = 0.00048;
+    xRatio = screenWidth/SENSITIVITY_LEVELS;
+    pRatio = 0.00048;
 
 
     if (hid_init()) {
@@ -207,7 +264,6 @@ int main(int argc, char* argv[]) {
         printf("device open\n");
     }
 
-
     // Set the hid_read() function to be non-blocking.
     hid_set_nonblocking(handle, 1);
 
@@ -216,41 +272,36 @@ int main(int argc, char* argv[]) {
     // non-blocking by the call to hid_set_nonblocking() above.
     // This loop demonstrates the non-blocking nature of hid_read().
     res = 0;
-    while (1) {
-
-        res = hid_read(handle, buf, sizeof(buf));
-        usleep(1200);
-        // Print out the returned buffer.
-        if(res > 0) {
-            for (i = 0; i < res; i++) {
-
-                xPen = calcPenValue((int) buf[2], (int) buf[3]);
-                yPen = calcPenValue((int) buf[4], (int) buf[5]);
-                pPen = calcPenValue((int) buf[6], (int) buf[7]);
-
-//                printf("%d %d\n", yPen);
-                if(calculateYMouse(yPen) > 0) {
-                    xMouse = xPen * xRatio;
-                    yMouse = calculateYMouse(yPen);
-                    pMouse = pPen * pRatio;
-
-                    mouseHandle(xMouse, yMouse, pMouse, (int) buf[1]);
-                }
-//                printf("mousepos: %d, %f, %f, %d, %f, %f \n",xPen, xRatio, xMouse, yPen, yRatio, yMouse);
 
 
-//                printf("%02hhx ", buf[i]);
-            }
 
-            printf("\n");
+    moveMouse = CGEventCreateMouseEvent(
+            NULL, kCGEventMouseMoved,
+            CGPointMake(0, 0),
+            kCGMouseButtonLeft // ignored
+    );
 
-        }
+    dragMouse = CGEventCreateMouseEvent(
+            NULL, kCGEventLeftMouseDragged,
+            CGPointMake(0, 0),
+            kCGMouseButtonLeft // ignored
+    );
+
+    clickLeftMouse = CGEventCreateMouseEvent(
+            NULL, kCGEventLeftMouseDown,
+            CGPointMake(0,0),
+            kCGMouseButtonLeft
+    );
+
+    releaseLeftMouse = CGEventCreateMouseEvent(
+            NULL, kCGEventLeftMouseUp,
+            CGPointMake(0, 0),
+            kCGMouseButtonLeft
+    );
+
+    while(1) {
+        handleTablet();
     }
-
-    hid_close(handle);
-
-    /* Free static HIDAPI objects. */
-    hid_exit();
 
     return 0;
 }
